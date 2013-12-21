2d28cedf581d16c9e4a1fbe30e0dfc5320f88af1
Chas Emerick <chas@cemerick.com>
2013-12-21 11:11:14 -0500

    0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cemerick/piggieback "0.1.2"
+(defproject com.cemerick/piggieback "0.1.3-SNAPSHOT"
   :description "Adding support for running ClojureScript REPLs over nREPL."
   :url "http://github.com/cemerick/piggieback"
   :license {:name "Eclipse Public License"
