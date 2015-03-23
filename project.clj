47362bb935558db49dc906e24c5d7ba9160e4d72
Chas Emerick <chas@cemerick.com>
2015-03-23 13:04:05 -0400

    rewrite of piggieback in terms of the new cljs.repl/repl*

--- project.clj
+++ project.clj
@@ -1,23 +1,19 @@
-(defproject com.cemerick/piggieback "0.1.6-SNAPSHOT"
+(defproject com.cemerick/piggieback "0.2.0-SNAPSHOT"
   :description "Adding support for running ClojureScript REPLs over nREPL."
   :url "http://github.com/cemerick/piggieback"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.nrepl "0.2.7"]
-                 [org.clojure/clojurescript "0.0-2665"]]
+                 [org.clojure/tools.nrepl "0.2.8"]
+                 [org.clojure/clojurescript "0.0-3147"]]
   
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   
   :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}}
 
-  ; restore testing under 1.5.1 when
-  ; https://github.com/clojure/clojurescript/commit/ab72c79f02a43670de63a0eea66c6baebc354e92#commitcomment-9216905
-  ; is fixed
   :aliases  {"all" ["with-profile" "dev"]}
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   
   ;;maven central requirements
   :scm {:url "git@github.com:cemerick/piggieback.git"}
--- test/cemerick/piggieback_test.clj
+++ test/cemerick/piggieback_test.clj
@@ -1,7 +1,8 @@
 (ns cemerick.piggieback-test
   (:require [cemerick.piggieback :as pb]
             [clojure.tools.nrepl :as nrepl]
-            (clojure.tools.nrepl [server :as server]))
+            (clojure.tools.nrepl [server :as server])
+            cljs.repl.nashorn)
   (:use clojure.test))
 
 (def ^:dynamic *server-port* nil)
@@ -23,7 +24,9 @@
           session (nrepl/client-session (nrepl/client conn Long/MAX_VALUE))]
       ; need to let the dynamic bindings get in place before trying to eval anything that
       ; depends upon those bingings being set
-      (doall (nrepl/message session {:op "eval" :code "(cemerick.piggieback/cljs-repl)"}))
+      (doall (nrepl/message session {:op "eval" :code (nrepl/code
+                                                        (cemerick.piggieback/cljs-repl
+                                                          (cljs.repl.nashorn/repl-env)))}))
       (try
         (binding [*server-port* port
                   *session* session]
