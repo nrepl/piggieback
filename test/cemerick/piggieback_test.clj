827dcd4b9f8153a217aa24623004a0c301f1edaf
Neil Okamoto <neil.okamoto@gmail.com>
2018-03-23 00:32:02 -0700

    Fixes for cljfmt

--- test/cemerick/piggieback_test.clj
+++ test/cemerick/piggieback_test.clj
@@ -11,8 +11,8 @@
   (assert (= ["user"]
              (filter identity
                      (map :ns (nrepl/message
-                                session
-                                {:op "eval" :code "clojure.core/*ns*"}))))))
+                               session
+                               {:op "eval" :code "clojure.core/*ns*"}))))))
 
 (def ^:private jdk8+ (try (import 'java.time.Instant)
                           true
@@ -22,11 +22,11 @@
   (if jdk8+
     (do (require 'cljs.repl.nashorn)
         (nrepl/code
-          (cemerick.piggieback/cljs-repl
-            (cljs.repl.nashorn/repl-env))))
+         (cemerick.piggieback/cljs-repl
+          (cljs.repl.nashorn/repl-env))))
     (nrepl/code
-      (cemerick.piggieback/cljs-repl
-        (cljs.repl.rhino/repl-env)))))
+     (cemerick.piggieback/cljs-repl
+      (cljs.repl.rhino/repl-env)))))
 
 (defn repl-server-fixture
   [f]
@@ -36,13 +36,13 @@
     (let [port (.getLocalPort (:ss @server))
           conn (nrepl/connect :port port)
           session (nrepl/client-session (nrepl/client conn Long/MAX_VALUE))]
-      ; need to let the dynamic bindings get in place before trying to eval anything that
-      ; depends upon those bingings being set
+      ;; need to let the dynamic bindings get in place before trying to eval anything that
+      ;; depends upon those bingings being set
       (doall (nrepl/message session {:op "eval" :code cljs-repl-start-code}))
       (try
         (binding [*server-port* port
                   *session* session]
-        (f))
+          (f))
         (finally
           (doall (nrepl/message session {:op "eval" :code ":cljs/quit"}))
           (assert-exit-ns session "user"))))))
@@ -50,45 +50,45 @@
 (use-fixtures :once repl-server-fixture)
 
 (deftest default-sanity
-  ; I think there's a race condition between when previous expressions are evaluated
-  ; (which nREPL serializes for a session) and when the next code to be evaluated is analyzed
-  ; in piggieback (which has no visibility into the session serialization mechanism). The
-  ; fix is to work like a REPL _should_, i.e. wait for the full response of an evaluation
-  ; prior to sending out another chunk of code.
+  ;; I think there's a race condition between when previous expressions are evaluated
+  ;; (which nREPL serializes for a session) and when the next code to be evaluated is analyzed
+  ;; in piggieback (which has no visibility into the session serialization mechanism). The
+  ;; fix is to work like a REPL _should_, i.e. wait for the full response of an evaluation
+  ;; prior to sending out another chunk of code.
   (doall (nrepl/message *session* {:op "eval" :code "(defn x [] (into [] (js/Array 1 2 3)))"}))
   (is (= [1 2 3] (->> {:op "eval" :code "(x)"} (nrepl/message *session*) nrepl/response-values first))))
 
 (deftest printing-works
   (is (= {:value ["nil"] :out "[1 2 3 4]\n"}
-        (-> (nrepl/message *session* {:op "eval" :code "(println [1 2 3 4])"})
-          nrepl/combine-responses
-          (select-keys [:value :out])))))
+         (-> (nrepl/message *session* {:op "eval" :code "(println [1 2 3 4])"})
+             nrepl/combine-responses
+             (select-keys [:value :out])))))
 
 (deftest proper-ns-tracking
   (is (= "cljs.user" (-> (nrepl/message *session* {:op "eval" :code "5"})
-                       nrepl/combine-responses
-                       :ns)))
+                         nrepl/combine-responses
+                         :ns)))
   (is (= "foo.bar" (-> (nrepl/message *session* {:op "eval" :code "(ns foo.bar)"})
                        nrepl/combine-responses
                        :ns)))
   (doall (nrepl/message *session* {:op "eval" :code "(defn ns-tracking [] (into [] (js/Array 1 2 3)))"}))
-  
+
   (is (= ["[1 2 3]"] (-> (nrepl/message *session* {:op "eval" :code "(ns-tracking)"})
-                       nrepl/combine-responses
-                       :value)))
-  
+                         nrepl/combine-responses
+                         :value)))
+
   ;; TODO emit a response message to in-ns, doesn't seem to hit eval....
   (is (= "cljs.user" (-> (nrepl/message *session* {:op "eval" :code "(in-ns 'cljs.user)"})
-                       nrepl/combine-responses
-                       :ns)))
+                         nrepl/combine-responses
+                         :ns)))
   (is (= "cljs.user" (-> (nrepl/message *session* {:op "eval" :code "(ns cljs.user)"})
-                       nrepl/combine-responses
-                       :ns)))
+                         nrepl/combine-responses
+                         :ns)))
   (let [resp (-> (nrepl/message *session* {:op "eval" :code "(ns-tracking)" :ns "foo.bar"})
-               nrepl/combine-responses)]
+                 nrepl/combine-responses)]
     (is (= ["[1 2 3]"] (:value resp)))
     (is (= "cljs.user" (:ns resp))))
-  
+
   (is (= "foo.bar" (-> (nrepl/message *session* {:op "eval" :code "(ns foo.bar)" :ns "cljs.user"})
-                     nrepl/combine-responses
-                     :ns))))
+                       nrepl/combine-responses
+                       :ns))))
