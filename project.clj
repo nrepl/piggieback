f5dbef6fa2cb3eafc435146141c0ad0c38588af9
Bozhidar Batsov <bozhidar@batsov.com>
2018-04-18 22:59:52 +0300

    Bump the nREPL dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+  :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]]
 
@@ -16,7 +16,7 @@
   :aliases  {"all" ["with-profile" "dev"]}
 
   ;; painful for users; https://github.com/technomancy/leiningen/issues/1771
-  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
                    :source-paths ["src" "dev"]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [org.clojure/clojurescript "1.9.946" :scope "provided"]]}
