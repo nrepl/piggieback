e342f9751f877fc921200a8daf105352755f4a4e
Bozhidar Batsov <bozhidar@batsov.com>
2018-04-18 23:16:51 +0300

    Bump the development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/piggieback "0.3.1"
+(defproject cider/piggieback "0.3.2-SNAPSHOT"
   :description "Adding support for running ClojureScript REPLs over nREPL."
   :url "http://github.com/clojure-emacs/piggieback"
   :license {:name "Eclipse Public License"
