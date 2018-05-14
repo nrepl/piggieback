a25f11ce251417f36bdd5e79ff34fb39c3566fc9
Bruce Hauman <bhauman@gmail.com>
2018-05-14 14:36:00 -0400

    add development helper in user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -2,10 +2,14 @@
   (:require
    [cider.piggieback :as pback]
    [cljs.repl :as repl]
-   [cljs.repl.nashorn :as nash]))
+   [cljs.repl.nashorn :as nash]
+   [cljs.repl.node :as node]))
 
 (defn cljs []
   (pback/cljs-repl (nash/repl-env :verbose true :repl-verbose true)))
+
+(defn cljs-node []
+  (pback/cljs-repl (node/repl-env)))
 
 (defn cljs* []
   (repl/repl (nash/repl-env :verbose true :repl-verbose true)))
