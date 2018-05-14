d312b79d43f97700976b028f5b569ab846350690
Bruce Hauman <bhauman@gmail.com>
2018-05-14 14:35:21 -0400

    bump to 0.3.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/piggieback "0.3.3"
+(defproject cider/piggieback "0.3.4-SNAPSHOT"
   :description "Adding support for running ClojureScript REPLs over nREPL."
   :url "http://github.com/clojure-emacs/piggieback"
   :license {:name "Eclipse Public License"
