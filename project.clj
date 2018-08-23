c7eaed5a673b14e1111698b6d93e1a36e9bea296
Bruce Hauman <bhauman@gmail.com>
2018-08-23 10:28:24 -0400

    bump to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/piggieback "0.3.8"
+(defproject cider/piggieback "0.3.9-SNAPSHOT"
   :description "Middleware adding support for running ClojureScript REPLs over nREPL."
   :url "http://github.com/nrepl/piggieback"
   :license {:name "Eclipse Public License"
