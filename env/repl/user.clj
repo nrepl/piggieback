b8b80235f58bf6785f80e6c7d9df9d7850098bcb
Shen Tian <biozinc@gmail.com>
2020-05-10 18:25:19 +0200

    Patch up user.clj

--- env/repl/user.clj
+++ env/repl/user.clj
@@ -13,15 +13,12 @@
     `(do)))
 
 (when-ns cljs.repl
- (require 'cljs.repl
-          '[cljs.repl.nashorn :as nash]
-          '[cljs.repl.node :as node])
+  (require '[cljs.repl :as repl]
+           '[cljs.repl.node :as node])
 
- (defn cljs []
-   (pback/cljs-repl (nash/repl-env :verbose true :repl-verbose true)))
 
  (defn cljs-node []
    (pback/cljs-repl (node/repl-env)))
 
  (defn cljs* []
-   (repl/repl (nash/repl-env :verbose true :repl-verbose true))))
+   (repl/repl (node/repl-env))))
